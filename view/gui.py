94865c9de3bb1a81125c1992a22977cd871293d0
muditchaudhary <muditchaudhary7@gmail.com>
2022-11-29 22:36:36 -0500

    Refine GUI | Fix minor bugs

--- view/gui.py
+++ view/gui.py
@@ -5,51 +5,67 @@ import networkx as nx
 import leafmap.foliumap as leafmap
 import json
 
+
 class gui:
-    def __init__(self, filename = "config.json"):
+    def __init__(self, filename="config.json"):
         with open(filename) as config_file:
             self.config = json.load(config_file)
-    
+            self.address_to = ""
+            self.address_from = ""
+            self.transport = self.config['TRAVEL_MODE'][0]
+            self.basemap = self.config['BASEMAPS'][0]
+            self.tolerance = 1.0
+
+            st.set_page_config(layout="wide")
+            hide_menu_style = """
+                    <style>
+                    #MainMenu {visibility: hidden;}
+                    </style>
+                    """
+            st.markdown(hide_menu_style, unsafe_allow_html=True)
+            lat, lon = 42.4529, -72.5653
+            self.map = leafmap.Map(center=(lat, lon), zoom=16)
+            self.map.add_basemap(self.basemap)
+            self.map.to_streamlit()
+
+    def generate_and_show_gui(self):
+
+        with st.sidebar:
+            st.markdown("<h1 style='text-align: center;font-size:60px;'>🧭</h1>", unsafe_allow_html=True)
+            st.markdown("<h1 style='text-align: center;'>EleNa: Elevation Based Navigation System</h1>",
+                        unsafe_allow_html=True)
+
+            self.basemap = st.selectbox("Map Type", self.config['BASEMAPS'])
+            if self.basemap in self.config['BASEMAPS'][1:]:
+                self.basemap = self.basemap.upper()
+
+            self.transport = st.selectbox("Method of Transportation", self.config['TRAVEL_MODE'])
+
+            self.address_from = st.text_input("Go from", key="go_from")
+            self.address_to = st.text_input("Go to", key="go_to")
+
+            st.markdown("<h3 style='text-align: center;'>Tolerance</h3>", unsafe_allow_html=True)
+            self.tolerance = st.slider(label="", min_value=1.0, max_value=5.0, label_visibility="collapsed", step=0.1,
+                                       format="%f times")
+            st.write("Shows path upto " + str(self.tolerance) + " times the shortest path")
+
+            with st.columns(3)[1]:
+                st.button("Clear", on_click=self.clear_text)
+
+        with st.columns(7)[3]:
+            st.button("Get Directions", on_click=self.get_directions, type="primary")
+
+    def get_directions(self):
+        if self.address_from == "" or self.address_to == "" or self.address_from == self.address_to:
+            st.warning(" Invalid Source or Destination!", icon="⚠️")
+        else:
+            with st.spinner("Generating shortest route"):
+                import time
+                time.sleep(10)
+                #TODO: Add function for getting shortest path
+
+            st.success('Shortest route found', icon="✅")
+
     def clear_text(self):
         st.session_state["go_from"] = ""
         st.session_state["go_to"] = ""
-
-    def generate_and_show_gui(self):
-        with st.sidebar:
-            st.markdown("<h1 style='text-align: center;font-size:100px;'>🧭</h1>", unsafe_allow_html=True)
-            st.markdown("<h1 style='text-align: center;'>EleNa: Elevation Based Navigation System</h1>", unsafe_allow_html=True)
-
-            basemap = st.selectbox("Select Map Type", self.config['BASEMAPS'])
-            if basemap in self.config['BASEMAPS'][:-1]:
-                basemap=basemap.upper()
-            
-            transport = st.selectbox("Choose transport", self.config['TRAVEL_MODE'])
-            
-            self.address_from = st.text_input("Go from", key="go_from")
-            self.address_to = st.text_input("Go to", key="go_to")
-    
-            st.markdown("<h3 style='text-align: center;'>Tolerance</h3>", unsafe_allow_html=True)
-            tolerance = st.slider('Select tolerance for elevation', min_value = 1.0, max_value = 5.0)
-            st.write("Shows path upto "+ str(tolerance) +" times the shortest path")
-            
-            col1, col2 = st.columns([1,1])
-            with col1:
-                st.button("Clear Choice", on_click=clear_text)
-            with col2:
-                st.button("Get Directions", on_click=get_directions)
-        
-        lat, lon = 42.4529, -72.5653
-        m = leafmap.Map(center=(lat, lon), zoom=16)
-        m.add_basemap(basemap)
-        m.to_streamlit()
-        
-    def get_directions(self):
-        if (self.address_from != "" and self.address_to != ""):
-            if (self.address_from != self.address_to):
-                st.write("Valid Source and Destination!")
-            else:
-                st.write("Invalid Source and Destination!")
-        else:
-            st.write("Invalid Source and Destination!")
-
-
